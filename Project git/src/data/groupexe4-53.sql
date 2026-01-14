4af6896c1bcc03a67db1c9a3e8660468bdac9dd1
hhadi-dei <hazem.hadi@appliedtechnology.se>
2026-01-14 13:37:49 +0100

    added ex 5.3

---
+++ Project git/src/data/groupexe4-53.sql
@@ -0,0 +1,12 @@
+WITH product_sales AS (
+    -- Step 1: Calculate total sales for every individual product
+    SELECT 
+        product_id,
+        SUM(quantity * price_at_purchase) AS product_revenue,
+        PERCENT_RANK() OVER (ORDER BY SUM(quantity * price_at_purchase) DESC) AS sales_rank
+    FROM order_items
+    GROUP BY product_id
+)
+SELECT 
+    ROUND((SUM(product_revenue) FILTER (WHERE sales_rank <= 0.10) / SUM(product_revenue)),3) * 100 AS percentage_contribution
+FROM product_sales;
