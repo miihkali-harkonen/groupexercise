97c9dbcd27c2008b0b9e032cb0f5f10b3d75d703
Fariba Kamani <175831925+Fariba-Kamani@users.noreply.github.com>
2026-01-14 10:09:41 +0100

    Add query to count products with low stock

--- Project git/src/data/queries.py
+++ Project git/src/data/queries.py
@@ -10,3 +10,25 @@ def connect():
     except (Exception, DatabaseError) as error:
         print("DB connection error:", error)
         return None
+
+def count_low_stock_products():
+    conn = connect()
+    if conn == None:
+        return
+    try:
+        cur = conn.cursor()
+        cur.execute("""SELECT COUNT(*) AS low_stock_products
+                        FROM products
+                        WHERE stock_quantity < 10
+                    """)
+        low_stock_count = cur.fetchone()[0]
+        print(f"THe count of products with stock quantity less than 10 is {low_stock_count}")
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if cur is not None:
+            cur.close()
+        if conn is not None:
+            conn.close()
+if __name__ == "__main__":
+    count_low_stock_products()
