bfe8ba2254aac144744d70e9807ccd0551980609
hhadi-dei <hazem.hadi@appliedtechnology.se>
2026-01-15 01:10:45 +0100

    readability

--- Project git/src/data/groupexe4-53.sql
+++ Project git/src/data/groupexe4-53.sql
@@ -1,3 +1,4 @@
+
 WITH product_sales AS (
     SELECT 
         product_id,
@@ -14,3 +15,13 @@ SELECT
 		NULLIF(SUM(product_revenue)), 3)
 		) * 100 AS percentage_contribution
 FROM product_sales;
+
+
+    SELECT 
+        product_id,
+        SUM(quantity * price_at_purchase) AS product_revenue,
+        PERCENT_RANK() OVER (
+			ORDER BY SUM(quantity * price_at_purchase) DESC
+			) AS sales_rank
+    FROM order_items
+    GROUP BY product_id
