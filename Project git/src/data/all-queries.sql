a429d58c7f72098582b5902ab9af2637ee39b0ca
hhadi-dei <hazem.hadi@appliedtechnology.se>
2026-01-15 04:25:32 +0100

    fix typo

--- Project git/src/data/all-queries.sql
+++ Project git/src/data/all-queries.sql
@@ -34,8 +34,7 @@ FROM order_items
 GROUP BY order_id)
 AS sub;
 
--- 2.3 Monthly breakdown of the number of orders.
-and total sales
+-- 2.3 Monthly breakdown of the number of orders and total sales
 SELECT
 	DATE_TRUNC('month', orders.order_date)::DATE as month,
 	COUNT(orders.order_id) AS total_orders,
@@ -111,7 +110,6 @@ WHERE
     );
 
 
-SELECT product_id FROM products
 -- 4.1 Products that have never been ordered.
 SELECT
     products.product_id,
@@ -184,16 +182,34 @@ WITH product_sales AS (
         product_id,
         SUM(quantity * price_at_purchase) AS product_revenue,
         PERCENT_RANK() OVER (
-			ORDER BY SUM(quantity * price_at_purchase) DESC
-			) AS sales_rank
+            ORDER BY SUM(quantity * price_at_purchase) DESC
+        ) AS sales_rank
     FROM order_items
     GROUP BY product_id
 )
 SELECT 
     ROUND(
-		(SUM(product_revenue) FILTER (WHERE sales_rank <= 0.10) / 
-		NULLIF(SUM(product_revenue)), 3)
-		) * 100 AS percentage_contribution
+        SUM(product_revenue) FILTER (WHERE sales_rank <= 0.10) / 
+        SUM(product_revenue), 3) * 100 AS percentage_contribution
+FROM product_sales;
+
+
+-- 5.3 Percentage of total sales attributed to the top 10% of products by sales.
+
+WITH product_sales AS (
+    SELECT 
+        product_id,
+        SUM(quantity * price_at_purchase) AS product_revenue,
+        PERCENT_RANK() OVER (
+            ORDER BY SUM(quantity * price_at_purchase) DESC
+        ) AS sales_rank
+    FROM order_items
+    GROUP BY product_id
+)
+SELECT 
+    ROUND(
+        SUM(product_revenue) FILTER (WHERE sales_rank <= 0.10) / 
+        SUM(product_revenue), 3) * 100 AS percentage_contribution
 FROM product_sales;
 
 -- visualization of 5.3
